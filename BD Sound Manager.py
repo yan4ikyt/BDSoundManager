926f5be19a102e39cfed9d87c71413c0b214ebe7
yan4ikyt <59663838+yan4ikyt@users.noreply.github.com>
2021-05-02 14:19:47 +0300

    Add files via upload
    BD Sound Manager V.1.1
    -Fixed bugs
    -Added converter to WAV 
    
    (Thanks to pips for converter)

--- BD Sound Manager.py
+++ BD Sound Manager.py
@@ -8,16 +8,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-bd", "--bd", help=".BD File")
 parser.add_argument("-hd", "--hd", help=".HD File")
 parser.add_argument("-o", "--out", help="Output folder (must exist)")
+parser.add_argument("-inf", "--inputfolder", help="Input folder (must exist)")
 parser.add_argument("-in", "--input", help="Input VAG file for export")
 parser.add_argument("-m", "--mode", help="'E' For extract, 'i' For import, 'F' for fix VAG file, 'l' for list audio in BD file")
-parser.add_argument("-i", "--ind", help="Index of VAG", type=int)
 args = parser.parse_args()
 if args.mode == None:
     if args.bd == None or args.hd == None:
         print ("""
-    BD Sound Manager v.1.0
+    BD Sound Manager v.1.1
     (for Parappa The Rapper 2)
-    Version 1.0
+    Version 1.1
     Original code by Nisto
     Modificated programm by Yan4ik
     
@@ -25,7 +25,7 @@ if args.mode == None:
     Please run programm always in CMD
         """)
         time.sleep(5)
-        exit()
+        quit()
 def fixvag (file):
     # By Yan4ik / Original code by pips
     # read file from buffer
@@ -179,68 +179,104 @@ if mode == 'e':
 
         with open("%s_%02d.VAG" % (out_stem, vag_index), "wb") as vag:
             vag.write(header + bdbuf[vag_offset:vag_offset+vag_size])
-
+        ifilepath = "\"%s_%02d.VAG\"" % (out_stem, vag_index)
+        efilepath = "\"%s_%02d.wav\"" % (out_stem, vag_index)
+        args = " /OTWAVU /OF48000 /IF24000 " + ifilepath + " " + efilepath
+        print("Converting file | MFAudio v.1.0", end="")
+        subprocess.call("mfaudio " + args)
+        print("Done.")
+        os.remove("%s_%02d.VAG"% (out_stem, vag_index)) 
 elif mode == 'i':
-    in_vag_path = args.input
-    if not os.access(in_vag_path, os.R_OK):
-        print("Could not open .VAG file")
+    in_dir = args.inputfolder
+    hd_path = args.hd
+    bd_path = args.bd
+    if not os.access(in_dir, os.R_OK):
+        input("Can not read the specified folder!")
         sys.exit(1)
-    with open(in_vag_path, "rb") as in_vagf:
-        in_vag_header = in_vagf.read(0x30)
-        # body_size = get_u32_be(in_vag_buf, 0x0C)
-        in_vag_rate = get_u32_be(in_vag_header, 0x10)
-        in_adpcm_buf = in_vagf.read()
+    directory = os.fsencode(in_dir)
+    target_vag_index = 0
+    for file in os.listdir(directory):
+        if os.path.splitext(file)[1] == b".wav":
+          with open(hd_path, "rb") as hd:
+              hdbuf = bytearray( hd.read() )
 
-    in_adpcm_size = len(in_adpcm_buf)
-
-    target_vag_index = args.ind
-    if target_vag_index > max_vag_index:
-        print("Specified sample index exceeds max index")
-        sys.exit(1)
-
-    target_vag_offset = get_vag_offset(hdbuf, vagi_chunk_offset, target_vag_index)
-    if target_vag_index < max_vag_index:
-        target_vag_size = get_vag_offset(hdbuf, vagi_chunk_offset, target_vag_index+1) - target_vag_offset
-    else:
-        target_vag_size = bd_size - target_vag_offset
-
-    # update sample rate
-    put_vag_sample_rate(hdbuf, vagi_chunk_offset, target_vag_index, in_vag_rate)
-
-    # update offsets for subsequent samples
-    for sub_vag_index in range(target_vag_index+1, max_vag_index+1):
-        sub_vag_offset = get_vag_offset(hdbuf, vagi_chunk_offset, sub_vag_index)
-        put_vag_offset(hdbuf, vagi_chunk_offset, sub_vag_index, (sub_vag_offset - target_vag_size) + in_adpcm_size)
-
-    # update BD size
-    put_u32_le(hdbuf, 0x20, (bd_size - target_vag_size) + in_adpcm_size)
-
-    with open(hd_path, "wb") as hd:
-        hd.write(hdbuf)
-
-    with open(bd_path, "wb") as bd:
-        bd.write(bdbuf[:target_vag_offset])
-        bd.write(in_adpcm_buf)
-        bd.write(bdbuf[target_vag_offset+target_vag_size:bd_size])
-        bd.truncate()
-     
-    needle = b"\x00\x07\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77"
-
-    with open(bd_path, "rb+") as file:
-        data = file.read()
-    
-        i = 0
-        while True:
-            i = data.find(needle, i)
-            if i == -1:
-                break
+          with open(bd_path, "rb") as bd:
+              bdbuf = bytearray( bd.read() )
         
-            file.seek(i - 15)
-            if file.peek(1) != b"\x01":
-                file.write(b"\x01")
-                print('Writed 1 flag | BD Fix v.1.0 ')
+          bd_size = get_u32_le(hdbuf, 0x20)
+          vagi_chunk_offset = get_u32_le(hdbuf, 0x30)
+          max_vag_index = get_u32_le(hdbuf, vagi_chunk_offset + 0x0C)
+        
+          filename = os.fsdecode(file)
+          ifilepath = "\"%s\"" % (os.path.join(in_dir, filename))
+          efilepath = "\"%s_%02d.VAG\"" % (os.path.join(in_dir, os.path.splitext(filename)[0]), target_vag_index)
+          args = " /OTVAGC /OF24000 " + ifilepath + " " + efilepath
+          print("Converting file | MFAudio v.1.0 ...", end="")
+          subprocess.call("mfaudio " + args)
+          print("Done. \nExecuting BD Fix v.1.1 :")
+          in_vag_path = os.path.join(in_dir, os.path.splitext(filename)[0]) + "_%02d.VAG" % (target_vag_index)
+          needle = b"\x00\x07\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77\x77"
 
-            i += 1
+          with open(in_vag_path, "rb+") as file:
+            data = file.read()
+    
+            i = 0
+            while True:
+              i = data.find(needle, i)
+              if i == -1:
+                  break
+        
+              file.seek(i - 15)
+              if file.peek(1) != b"\x01":
+                  file.write(b"\x01")
+                  print('Writed 1 flag | BD Fix v.1.1')
+
+              i += 1
+        
+          #print(in_vag_path)
+        
+          with open(in_vag_path, "rb") as in_vagf:
+              in_vag_header = in_vagf.read(0x30)
+              # body_size = get_u32_be(in_vag_buf, 0x0C)
+              in_vag_rate = get_u32_be(in_vag_header, 0x10)
+              in_adpcm_buf = in_vagf.read()
+        
+
+          in_adpcm_size = len(in_adpcm_buf)
+
+          if target_vag_index > max_vag_index:
+              input("Specified sample index exceeds max index: ")
+              sys.exit(1)
+
+          target_vag_offset = get_vag_offset(hdbuf, vagi_chunk_offset, target_vag_index)
+          if target_vag_index < max_vag_index:
+              target_vag_size = get_vag_offset(hdbuf, vagi_chunk_offset, target_vag_index+1) - target_vag_offset
+          else:
+              target_vag_size = bd_size - target_vag_offset
+
+          # update sample rate
+          put_vag_sample_rate(hdbuf, vagi_chunk_offset, target_vag_index, in_vag_rate)
+
+          # update offsets for subsequent samples
+          for sub_vag_index in range(target_vag_index+1, max_vag_index+1):
+              sub_vag_offset = get_vag_offset(hdbuf, vagi_chunk_offset, sub_vag_index)
+              put_vag_offset(hdbuf, vagi_chunk_offset, sub_vag_index, (sub_vag_offset - target_vag_size) + in_adpcm_size)
+
+          # update BD size
+          put_u32_le(hdbuf, 0x20, (bd_size - target_vag_size) + in_adpcm_size)
+
+          with open(hd_path, "wb") as hd:
+              hd.write(hdbuf)
+
+          with open(bd_path, "wb") as bd:
+              bd.write(bdbuf[:target_vag_offset])
+              bd.write(in_adpcm_buf)
+              bd.write(bdbuf[target_vag_offset+target_vag_size:bd_size])
+              bd.truncate()
+        
+          print("Successfully injected " + filename + " at bd index: " + str(target_vag_index))
+          target_vag_index += 1
+          os.remove(in_vag_path)
 elif args.mode == 'l':
     bd_stem = os.path.splitext(bd_path)[0]
 
@@ -269,10 +305,10 @@ elif args.mode == 'l':
  
 print ('''
 Done! 
-BD Sound Manager v.1.0
+BD Sound Manager v.1.1
 Mod by Yan4ik
 Original code by Nisto
 
 Original code BDFix by pips
 ''')
-exit();
+quit()
